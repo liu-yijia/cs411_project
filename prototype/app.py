f319531883411d117464d5b7c1afda8e1860d73c
liu-yijia <66379272+liu-yijia@users.noreply.github.com>
2021-03-30 01:17:42 -0400

    Update app.py

--- prototype/app.py
+++ prototype/app.py
@@ -5,5 +5,8 @@ from uber_rides.client import UberRidesClient
 session = Session(server_token=<TOKEN>)
 client = UberRidesClient(session)
 
-
 app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
